9ebf88e378b3d3cb6d3671300a4249695753d52c
Seung Park <separk@pugetsound.edu>
2021-10-12 10:09:58 -0700

    updated <head> tag to be correct

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
 /**
  * Scoreboard Homework
- * @author 
+ * @SeungPark
  */
+let score0 = document.querySelector("td[name='awayScore0']");
+score0.innerHTML = '1';
+let score1 = document.querySelector("td[name='awayScore1']");
+score1.innerHTML = '1';
+let score2 = document.querySelector("td[name='awayScore2']");
+score2.innerHTML = '5';
+let score3 = document.querySelector("td[name='awayScore3']");
+score3.innerHTML = '1';
+let score4 = document.querySelector("td[name='awayScore4']");
+score4.innerHTML = '3';
+let score5 = document.querySelector("td[name='awayScore5']");
+score5.innerHTML = '1';
+let score6 = document.querySelector("td[name='awayScore6']");
+score6.innerHTML = '6';
+
+/** Control Center */
+
+
+// let runUp = document.querySelector("#up");
+// runUp.addEventListener("click", function() {
+//     let who = document.querySelector("#who");
+//     let inning = document.querySelector("#inning");
+
+// }
+// )
+
+// function runTotal(){
+
+// }
